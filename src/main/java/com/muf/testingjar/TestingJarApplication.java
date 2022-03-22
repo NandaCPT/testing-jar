564c48e94a2854d2c224112685be53d6ed19b9a3
nanda <nanda.putra@muf.co.id>
2022-03-22 14:23:13 +0700

    first commit

---
+++ src/main/java/com/muf/testingjar/TestingJarApplication.java
@@ -0,0 +1,13 @@
+package com.muf.testingjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muf/testingjar/TestingJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muf.testingjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
